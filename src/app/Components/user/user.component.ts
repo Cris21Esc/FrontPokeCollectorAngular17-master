8200d7c9a2f75db19e66ed9e7c39d88e110af995
Cristopher <caescalante@ccoo.es>
2024-05-21 11:55:57 +0200

    actualización equipos pokemon (modificacion y creacion)

--- src/app/Components/user/user.component.ts
+++ src/app/Components/user/user.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ServicepokemonsService} from "../../service-pokemons.service";
-import { Router } from '@angular/router';
-import { ServiceusersService} from "../../service-users.service";
-import { EquipoPokemon} from "../../equipo-pokemon";
+import {Component, OnInit} from '@angular/core';
+import {ServicepokemonsService} from "../../service-pokemons.service";
+import {Router} from '@angular/router';
+import {ServiceusersService} from "../../service-users.service";
+import {EquipoPokemon} from "../../equipo-pokemon";
 
 @Component({
   selector: 'app-user',
@@ -13,121 +13,135 @@ import { EquipoPokemon} from "../../equipo-pokemon";
 export class UserComponent implements OnInit {
 
   token: string | null = null;
-
+  toggle: boolean = false;
   userInfo: any;
 
   caughtPokemons: string[] = [];
 
-  iduser:number|undefined;
+  iduser: number | undefined;
 
-  username:string|null="";
+  username: string | null = "";
 
-  stringid:string="";
-  mensajeBack:string="";
-  equipoCrear:number|undefined;
-  numEquipos:number=1;
-  equiposBack:EquipoPokemon[]=[];
-  equipos:number = 1;
+  stringid: string = "";
+  mensajeBack: string = "";
+  equipoCrear: number | undefined;
+  numEquipos: number = 1;
+  equiposBack: EquipoPokemon[] = [];
+  equipos: number = 1;
   pokesInEquipo: number = 0;
-  pokesArray: number[] =[];
+  pokesArray: number[] = [];
   private maxPokes: number = 6;
   slotIds: string[] = ["t1", "t2", "t3", "t4", "t5", "t6"];
 
-  constructor(private servicepokemons:ServicepokemonsService,private router: Router,private  serviceUser:ServiceusersService) { }
+  constructor(private servicepokemons: ServicepokemonsService, private router: Router, private serviceUser: ServiceusersService) {
+  }
 
   ngOnInit(): void {
     this.token = sessionStorage.getItem('token');
     this.userInfo = this.servicepokemons.getUserInfo();
-    this.username=sessionStorage.getItem('user');
+    this.username = sessionStorage.getItem('user');
     this.showpokes(this.username)
     this.servicepokemons.findUserIdByNombre(sessionStorage.getItem('user')).subscribe(
-      (userId)=>{
-        this.iduser=userId;
-        this.servicepokemons.findAllEquiposByUserId(userId).subscribe(data=>{
+      (userId) => {
+        this.iduser = userId;
+        this.servicepokemons.findAllEquiposByUserId(userId).subscribe(data => {
           console.log(data);
-          if(data.length>=1){
-            data.forEach(equipo=>{
+          if (data.length >= 1) {
+            data.forEach(equipo => {
               this.equiposBack.push(equipo);
-              this.equiposBack.forEach(equipo=>{
+              this.equiposBack.forEach(equipo => {
                 this.addPokeToEquipo(equipo.pokemon1_id.toString());
                 this.addPokeToEquipo(equipo.pokemon2_id.toString());
                 this.addPokeToEquipo(equipo.pokemon3_id.toString());
                 this.addPokeToEquipo(equipo.pokemon4_id.toString());
                 this.addPokeToEquipo(equipo.pokemon5_id.toString());
                 this.addPokeToEquipo(equipo.pokemon6_id.toString());
+                // @ts-ignore
+                document.getElementById('nomEquipo').value = equipo.nombre;
               });
             });
             // @ts-ignore
-            /*document.getElementById('crearEquipoButton').disabled = true;*/
-          }else{
+            document.getElementById('crearEquipoButton').disabled = true;
+          } else {
             // @ts-ignore
             document.getElementById('crearEquipoButton').disabled = false;
           }
         });
       });
   }
-  showpokes(nombre:string|null){
-      if (this.userInfo) {
-        this.servicepokemons.obteneruserPokemons(nombre).subscribe(
-          (data: string[]) => {
-            this.caughtPokemons = data;
-          },
-          (error) => {
-            console.log("No se pudo obtener la lista de pokemons:", error);
-          }
-        );
-      }
-    }
 
-    navigatepokemon(){
-      this.router.navigate(["/pokemon"]);
-    }
-
-  deletepokemon(idpokemondelete:string){
-    this.servicepokemons.findUserIdByNombre(this.userInfo.nombreUsuario).subscribe(
-      (userId)=>{
-        this.iduser=userId;
-        this.stringid=this.iduser.toString();
-        this.serviceUser.deletepokemon(idpokemondelete,this.stringid).subscribe(
-        ()=>{
-          this.showpokes(this.username);
+  showpokes(nombre: string | null) {
+    if (this.userInfo) {
+      this.servicepokemons.obteneruserPokemons(nombre).subscribe(
+        (data: string[]) => {
+          this.caughtPokemons = data;
+        },
+        (error) => {
+          console.log("No se pudo obtener la lista de pokemons:", error);
         }
       );
+    }
+  }
+
+  navigatepokemon() {
+    this.router.navigate(["/pokemon"]);
+  }
+
+  deletepokemon(idpokemondelete: string) {
+    this.servicepokemons.findUserIdByNombre(this.userInfo.nombreUsuario).subscribe(
+      (userId) => {
+        this.iduser = userId;
+        this.stringid = this.iduser.toString();
+        this.serviceUser.deletepokemon(idpokemondelete, this.stringid).subscribe(
+          () => {
+            this.showpokes(this.username);
+          }
+        );
       })
   }
 
   addPokeToEquipo(poke: string) {
     if (this.pokesInEquipo < this.maxPokes) {
       this.pokesInEquipo++;
-      this.pokesArray.push(parseInt(poke));
+      this.addElementToNullPosition(poke)
 
       // Encontrar el primer slot disponible
       for (let i = 0; i < this.maxPokes; i++) {
-          let slotId = this.slotIds[i];
-          let slotImg = document.querySelector(`#${slotId} .pokemon-image-container img`) as HTMLImageElement;
-          if (slotImg && slotImg.src.includes("huebo.png")) {
-              // Cambiar la fuente de la imagen del Pokémon
-              slotImg.src = `/assets/OnBattle/${poke}.gif`;
+        let slotId = this.slotIds[i];
+        let slotImg = document.querySelector(`#${slotId} .pokemon-image-container img`) as HTMLImageElement;
+        if (slotImg && slotImg.src.includes("huebo.png")) {
+          // Cambiar la fuente de la imagen del Pokémon
+          slotImg.src = `/assets/OnBattle/${poke}.gif`;
 
-              // Agregar un event listener para eliminar la imagen
-              slotImg.addEventListener("click", () => {
-                  this.remove(slotImg);
-              });
-              break;
-          }
+          // Agregar un event listener para eliminar la imagen
+          slotImg.addEventListener("click", () => {
+            if (this.toggle) {
+              this.remove(slotImg);
+            }
+          });
+
+          let slotDiv = document.querySelector(`#${slotId} #pokemonPlaceholder`) as HTMLDivElement;
+          this.servicepokemons.findpokemonbyid(poke).subscribe(
+            (response) => {
+              slotDiv.innerHTML = response.nombre;
+              slotImg.id = response.id;
+            });
+          break;
+        }
       }
-  } else {
+    } else {
       let errorPokes = document.getElementById("errorPokes");
       if (errorPokes) {
-          // Manejar el error (ej. mostrar un mensaje de error)
-          errorPokes.innerText = "No puedes agregar más de 6 Pokémon al equipo.";
+        // Manejar el error (ej. mostrar un mensaje de error)
+        errorPokes.innerText = "No puedes agregar más de 6 Pokémon al equipo.";
       }
+    }
   }
-}
 
   guardarEquipo() {
     let equipo = new EquipoPokemon();
-    equipo.user_id = <number> this.iduser;
+
+    equipo.user_id = this.iduser;
     // @ts-ignore
     equipo.nombre = document.getElementById('nombreEquipoPokemon').value;
     equipo.pokemon1_id = this.pokesArray[0];
@@ -137,20 +151,46 @@ export class UserComponent implements OnInit {
     equipo.pokemon5_id = this.pokesArray[4];
     equipo.pokemon6_id = this.pokesArray[5];
     console.log(equipo);
-    this.serviceUser.guardarTeam(equipo).subscribe((message)=>{
-      // @ts-ignore
-      document.getElementById("crearEquipoDiv").classList.add("d-none");
+    this.serviceUser.guardarTeam(equipo).subscribe((message) => {
       console.log(message.message);
     });
+    this.toggle = false;
   }
 
   crearEquipo() {
+    this.toggle = true;
     // @ts-ignore
     document.getElementById("crearEquipoDiv").classList.remove("d-none");
     this.equipoCrear = this.numEquipos;
   }
+
   remove(img: HTMLImageElement) {
     img.src = "/assets/objetos/huebo.png";
+    // @ts-ignore
+    let idPoke = img.parentElement.parentElement.id;
+    this.pokesArray[this.slotIds.indexOf(idPoke)] = -2;
+    // @ts-ignore
+    let slotDiv = img.parentElement.parentElement.querySelector('#pokemonPlaceholder');
+    // @ts-ignore
+    slotDiv.innerHTML = "???";
     this.pokesInEquipo--;
-}
+  }
+
+  modificarEquipo() {
+    this.toggle = true;
+  }
+
+  addElementToNullPosition(element: string) {
+    // Encuentra la primera posición que sea null
+    let nullIndex = this.pokesArray.indexOf(-2);
+
+    if (nullIndex !== -1) {
+      // Si se encontró una posición null, añade el elemento allí
+      this.pokesArray[nullIndex] = parseInt(element);
+    } else {
+      // Si no hay posiciones null, añade el elemento al final del array
+      this.pokesArray.push(parseInt(element));
+    }
+  }
+
 }
--- src/app/equipo-pokemon.ts
+++ src/app/equipo-pokemon.ts
@@ -1,83 +1,11 @@
-export class EquipoPokemon {
-  private _id: number | undefined;
-  private _user_id: number | undefined;
-  private _nombre: string | undefined;
-  private _pokemon1_id: number | undefined;
-  private _pokemon2_id: number | undefined;
-  private _pokemon3_id: number | undefined;
-  private _pokemon4_id: number | undefined;
-  private _pokemon5_id: number | undefined;
-  private _pokemon6_id: number | undefined;
+export class EquipoPokemon {  public id: number = 0;
+  public user_id: number | undefined = 0;
+  public nombre: string = "";
+  public pokemon1_id: number = 0;
+  public pokemon2_id: number = 0;
+  public pokemon3_id: number = 0;
+  public pokemon4_id: number = 0;
+  public pokemon5_id: number = 0;
+  public pokemon6_id: number = 0;
 
-  get id(): number {
-    return <number>this._id;
-  }
-
-  set id(value: number) {
-    this._id = value;
-  }
-
-  get user_id(): number {
-    return <number>this._user_id;
-  }
-
-  set user_id(value: number) {
-    this._user_id = value;
-  }
-
-  get nombre(): string {
-    return <string>this._nombre;
-  }
-
-  set nombre(value: string) {
-    this._nombre = value;
-  }
-
-  get pokemon1_id(): number {
-    return <number>this._pokemon1_id;
-  }
-
-  set pokemon1_id(value: number) {
-    this._pokemon1_id = value;
-  }
-
-  get pokemon2_id(): number {
-    return <number>this._pokemon2_id;
-  }
-
-  set pokemon2_id(value: number) {
-    this._pokemon2_id = value;
-  }
-
-  get pokemon3_id(): number {
-    return <number>this._pokemon3_id;
-  }
-
-  set pokemon3_id(value: number) {
-    this._pokemon3_id = value;
-  }
-
-  get pokemon4_id(): number {
-    return <number>this._pokemon4_id;
-  }
-
-  set pokemon4_id(value: number) {
-    this._pokemon4_id = value;
-  }
-
-  get pokemon5_id(): number {
-    return <number>this._pokemon5_id;
-  }
-
-  set pokemon5_id(value: number) {
-    this._pokemon5_id = value;
-  }
-
-  get pokemon6_id(): number {
-    return <number>this._pokemon6_id;
-  }
-
-  set pokemon6_id(value: number) {
-    this._pokemon6_id = value;
-  }
 }
