df2e13ab2dd4dd876f07319a19d1c04c50e1ae5e
Cristopher <caescalante@ccoo.es>
2024-05-28 16:08:04 +0200

    combate mas a medias

--- src/app/pokemon.ts
+++ src/app/pokemon.ts
@@ -1,122 +1,26 @@
 export class Pokemon {
 
-  private _id: number | undefined;
+  public id: number =0;
 
-  private _num_pokedex: string | undefined;
+  public num_pokedex: string = "0000";
 
-  private _nombre: string | undefined;
+  public nombre: string = "";
 
-  private _tipo: string | undefined;
+  public tipo: string = "";
 
-  private _rareza: string | undefined;
+  public rareza: string = "";
 
-  private _caught: boolean | undefined;
+  public caught: boolean = false;
 
-  private _hp: number | undefined;
+  public hp: number = -1;
 
-  private _atk: number | undefined;
+  public atk: number =-1;
 
-  private _atk_esp: number | undefined;
+  public atk_esp: number = -1;
 
-  private _def: number | undefined;
+  public def: number = -1;
 
-  private _def_esp: number | undefined;
+  public def_esp: number = -1;
 
-  get id(): number {
-    return <number>this._id;
-  }
-
-  set id(value: number) {
-    this._id = value;
-  }
-
-  get num_pokedex(): string {
-    return <string>this._num_pokedex;
-  }
-
-  set num_pokedex(value: string) {
-    this._num_pokedex = value;
-  }
-
-  get nombre(): string {
-    return <string>this._nombre;
-  }
-
-  set nombre(value: string) {
-    this._nombre = value;
-  }
-
-  get tipo(): string {
-    return <string>this._tipo;
-  }
-
-  set tipo(value: string) {
-    this._tipo = value;
-  }
-
-  get rareza(): string {
-    return <string>this._rareza;
-  }
-
-  set rareza(value: string) {
-    this._rareza = value;
-  }
-
-  get caught(): boolean {
-    return <boolean>this._caught;
-  }
-
-  set caught(value: boolean) {
-    this._caught = value;
-  }
-
-  get hp(): number {
-    return <number>this._hp;
-  }
-
-  set hp(value: number) {
-    this._hp = value;
-  }
-
-  get atk(): number {
-    return <number>this._atk;
-  }
-
-  set atk(value: number) {
-    this._atk = value;
-  }
-
-  get atk_esp(): number {
-    return <number>this._atk_esp;
-  }
-
-  set atk_esp(value: number) {
-    this._atk_esp = value;
-  }
-
-  get def(): number {
-    return <number>this._def;
-  }
-
-  set def(value: number) {
-    this._def = value;
-  }
-
-  get def_esp(): number {
-    return <number>this._def_esp;
-  }
-
-  set def_esp(value: number) {
-    this._def_esp = value;
-  }
-
-  get vel(): number {
-    return <number>this._vel;
-  }
-
-  set vel(value: number) {
-    this._vel = value;
-  }
-
-  private _vel: number | undefined;
+  public vel: number = -1;
 }
